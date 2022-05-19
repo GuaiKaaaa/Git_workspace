1cb474fddb3cd35c7fd736fb3c30df0409569464
GuaiKaaaa <471542104@qq.com>
2022-05-19 15:28:55 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/iflytek/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.iflytek.git;
+
+/**
+ * @author Guaika
+ * @creation 2022-05-19 15:20
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hellogit");
+    }
+}
